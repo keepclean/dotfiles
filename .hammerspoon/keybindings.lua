a8bbd370ba5ff55bb47590c565bcd8d966f61558
Rustam Iuldashev <r.uldashev@gmail.com>
2018-12-09 10:07:14 +0000

    Updated dotfiles

--- .hammerspoon/keybindings.lua
+++ .hammerspoon/keybindings.lua
@@ -1,12 +1,21 @@
 -- Different keybindings
 
+local function check_number(x)
+  if tonumber(x) ~= nil then
+    return x
+  else
+    return string.lower(x)
+  end
+end
+
 local function keyCode(key, mods, callback)
     mods = mods or {}
+    key = check_number(key)
     callback = callback or function() end
     return function()
-        hs.eventtap.event.newKeyEvent(mods, string.lower(key), true):post()
+        hs.eventtap.event.newKeyEvent(mods, key, true):post()
         hs.timer.usleep(1000)
-        hs.eventtap.event.newKeyEvent(mods, string.lower(key), false):post()
+        hs.eventtap.event.newKeyEvent(mods, key, false):post()
         callback()
     end
 end
@@ -16,3 +25,4 @@ local function remapKey(mods, key, keyCode)
 end
 
 remapKey({"ctrl"}, "m", keyCode("return"))
+remapKey({"ctrl"}, "'", keyCode(42, "shift")) -- "|" pipe
