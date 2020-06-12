d4d767b93bd5dd5418327216d0f0e3a2e65489e9
Rustam Iuldashev <r.uldashev@gmail.com>
2020-06-12 21:44:58 +0100

    update install script

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -1,37 +1,27 @@
 git clone --bare https://github.com/keepclean/dotfiles.git "${HOME}"/.dotfiles
 
-function config {
+config() {
    /usr/bin/git --git-dir="${HOME}"/.dotfiles/ --work-tree="${HOME}" "$@"
 }
 
+branch="macos"
 ostype="$(uname)"
 if [ "${ostype}" = "Linux" ]; then
     branch="linux"
-else
-    branch="macos"
-fi
-
-if config checkout "${branch}"; then
-    echo "Checked out dotfiles."
-else
-    echo "Backing up pre-existing dotfiles."
-    mkdir -p .dotfiles-backup
-    config checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I {} mv {} .dotfiles-backup/{}
 fi
 
 config checkout "${branch}"
 config config status.showUntrackedFiles no
 
-git clone --recursive https://github.com/zimfw/zimfw.git "${HOME}"/.zim
-
-if [ "${ostype}" = "Darwin" ]; then
-    VCS_DIR="${HOME}/Code/vcs"
-    test -d "${VCS_DIR}" || mkdir -p "${VCS_DIR}"
-    chdir "${VCS_DIR}"
-    git clone https://github.com/jonathanfilip/lucius.git
+if command -v zsh; then
+    curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+else
+    echo "zimfw will not be installed, install zsh first"
 fi
 
-if command -v zsh; then
-    echo "Change shell to zsh"
-    chsh -s "$(command -v zsh)"
+if [ "${ostype}" = "Darwin" ]; then
+    VCS_DIR="${HOME}/Code/src"
+    test -d "${VCS_DIR}" || mkdir -p "${VCS_DIR}"
+    path="github.com/jonathanfilip/lucius"
+    git clone https://"${path}".git "${VCS_DIR}/${path}"
 fi
